PhoshMobi/phosh-mobile-settings

commit f56f820810abf6af089fa30667aae268558ee29e

feedback-panel: Check for mobile friendly alarm app
If we find an alarm app that is likely to support feedbackd
we enable the sound settings row. This is currenlty based on
the app having the `--hidden` option. We might later on check
for the DBus name.

Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/270>

src/ms-feedback-panel.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
+ *               2023-2025 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -93,6 +94,7 @@ struct _MsFeedbackPanel {
   GtkWidget                 *prefer_flash;
 
   MsSoundRow                *alarm_clock_sound_row;
+  GCancellable              *check_alarm_app_cancel;
   GSoundContext             *sound_context;
   GCancellable              *sound_cancel;
 
@@ -733,6 +735,9 @@ ms_feedback_panel_dispose (GObject *object)
 {
   MsFeedbackPanel *self = MS_FEEDBACK_PANEL (object);
 
+  g_cancellable_cancel (self->check_alarm_app_cancel);
+  g_clear_object (&self->check_alarm_app_cancel);
+
   g_clear_object (&self->sound_cancel);
   g_clear_object (&self->sound_context);
 
@@ -824,6 +829,62 @@ ms_feedback_panel_init_audio (MsFeedbackPanel *self)
 
 
 static void
+on_check_alarm_app_ready (GObject *source_object, GAsyncResult *res, gpointer user_data)
+{
+  gboolean success;
+  MsFeedbackPanel *self;
+  g_autoptr (GError) err = NULL;
+  g_autoptr (GBytes) stdout_buf = NULL;
+  gboolean is_mobile_friendly = FALSE;
+  const char *output;
+
+  success = g_subprocess_communicate_finish (G_SUBPROCESS (source_object),
+                                             res,
+                                             &stdout_buf,
+                                             NULL,
+                                             &err);
+
+  if (!success) {
+    g_warning ("Failed to check for alarm app: %s", err->message);
+    return;
+  }
+
+  self = MS_FEEDBACK_PANEL (user_data);
+
+  output = g_bytes_get_data (stdout_buf, NULL);
+  /* If the alarm clock app supports --hidden it is very likely one with feedbackd support */
+  if (strstr (output, "--hidden ")) {
+    g_debug ("Found mobile friendly alarm app");
+    is_mobile_friendly = TRUE;
+  }
+
+  gtk_widget_set_visible (GTK_WIDGET (self->alarm_clock_sound_row), is_mobile_friendly);
+}
+
+
+static void
+ms_feedback_panel_check_alarm_app (MsFeedbackPanel *self)
+{
+  g_autoptr (GError) err = NULL;
+  g_autoptr (GSubprocess) clock = NULL;
+
+  clock = g_subprocess_new (G_SUBPROCESS_FLAGS_STDOUT_PIPE, &err, "gnome-clocks", "--help", NULL);
+  if (!clock) {
+    if (!g_error_matches (err, G_SPAWN_ERROR, G_SPAWN_ERROR_NOENT))
+      g_warning ("Failed to build alarm-clock subprocess: %s", err->message);
+    return;
+  }
+
+  self->check_alarm_app_cancel = g_cancellable_new ();
+  g_subprocess_communicate_async (clock,
+                                  NULL,
+                                  self->check_alarm_app_cancel,
+                                  on_check_alarm_app_ready,
+                                  self);
+}
+
+
+static void
 ms_feedback_panel_init (MsFeedbackPanel *self)
 {
   g_autoptr (GError) error = NULL;
@@ -863,6 +924,7 @@ ms_feedback_panel_init (MsFeedbackPanel *self)
     g_warning ("Failed to make sound context: %s", error->message);
 
   ms_feedback_panel_init_audio (self);
+  ms_feedback_panel_check_alarm_app (self);
 }
 
 

src/ui/ms-feedback-panel.ui
@@ -118,7 +118,7 @@
                         <child>
                           <object class="MsSoundRow" id="alarm_clock_sound_row">
                             <property name="visible">0</property>
-                            <property name="title" translatable="yes">Alarm clock</property>
+                            <property name="title" translatable="yes">Alarm Clock</property>
                             <property name="effect-name">alarm-clock-elapsed</property>
                           </object>
                         </child>

commit 3a672fb3ec565aa724d946ce1f888747a48d2c45

feedback-panel: Don't warn when operation got cancelled
This can happen in tests

Fixes: f56f820 ("feedback-panel: Check for mobile friendly alarm app")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/279>

src/ms-feedback-panel.c
@@ -845,7 +845,8 @@ on_check_alarm_app_ready (GObject *source_object, GAsyncResult *res, gpointer us
                                              &err);
 
   if (!success) {
-    g_warning ("Failed to check for alarm app: %s", err->message);
+    if (!success && !g_error_matches (err, G_IO_ERROR, G_IO_ERROR_CANCELLED))
+      g_warning ("Failed to check for alarm app: %s", err->message);
     return;
   }
 