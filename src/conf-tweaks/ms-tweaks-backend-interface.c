cc5c1dbc710fef15f2328607c80151048662a398
Stefan Hansson <newbyte@postmarketos.org>
2025-11-20 17:26:37 +0100

    conf-tweaks: Allow backend to request save as administrator
    Signed-off-by: Stefan Hansson <newbyte@postmarketos.org>
    Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/284>

--- src/conf-tweaks/ms-tweaks-backend-interface.c
+++ src/conf-tweaks/ms-tweaks-backend-interface.c
@@ -8,9 +8,28 @@
 
 #include "ms-tweaks-backend-interface.h"
 
+
+enum {
+  SIGNAL_SAVE_AS_ADMINISTRATOR,
+  SIGNAL_LAST,
+};
+guint signals[SIGNAL_LAST];
+
+
 G_DEFINE_INTERFACE (MsTweaksBackend, ms_tweaks_backend, G_TYPE_OBJECT)
 
 
 static void
 ms_tweaks_backend_default_init (MsTweaksBackendInterface *iface)
-{}
+{
+  signals[SIGNAL_SAVE_AS_ADMINISTRATOR] =
+    g_signal_new ("save-as-administrator",
+                  MS_TYPE_TWEAKS_BACKEND,
+                  G_SIGNAL_RUN_LAST,
+                  0,
+                  NULL, NULL, NULL,
+                  G_TYPE_NONE,
+                  2,
+                  G_TYPE_STRING, /* From. */
+                  G_TYPE_STRING); /* To. */
+}
