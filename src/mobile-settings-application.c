PhoshMobi/phosh-mobile-settings

commit 8e9df9dcf28191f22bfe04cf17b5fb108b1cf2c0

Add Troubleshooting Tab
These changes will add a troubleshooting tab in posh-mobile-settings.
This troubleshooting tab will contain debug information required
while reporting issues against this application.

Fixes: https://gitlab.gnome.org/guidog/phosh-mobile-settings/-/issues/30

src/mobile-settings-application.c
@@ -14,12 +14,17 @@
 #include "ms-plugin-loader.h"
 #include "ms-toplevel-tracker.h"
 #include "ms-head-tracker.h"
+#include "mobile-settings-debug-info.h"
 
 #include "protocols/wlr-foreign-toplevel-management-unstable-v1-client-protocol.h"
 #include "protocols/wlr-output-management-unstable-v1-client-protocol.h"
 
 #include <gdk/wayland/gdkwayland.h>
+#include <wayland-client.h>
 #include <glib/gi18n.h>
+
+#define PHOC_LAYER_SHELL_EFFECTS_PROTOCOL_NAME "zphoc_layer_shell_effects_v1"
+#define PHOSH_PRIVATE_PROTOCOL_NAME "phosh_private"
 
 enum {
   PROP_0,
@@ -40,6 +45,8 @@ struct _MobileSettingsApplication {
   struct wl_registry *wl_registry;
   struct zwlr_foreign_toplevel_manager_v1 *foreign_toplevel_manager;
   struct zwlr_output_manager_v1 *output_manager;
+  uint32_t    phoc_layer_shell_effects_version;
+  uint32_t    phosh_private_version;
   MsToplevelTracker  *toplevel_tracker;
   MsHeadTracker     *head_tracker;
 };
@@ -85,6 +92,10 @@ registry_handle_global (void               *data,
   } else if (strcmp (interface, zwlr_output_manager_v1_interface.name) == 0) {
     self->output_manager =
       wl_registry_bind (registry, name, &zwlr_output_manager_v1_interface, 2);
+  } else if (strcmp (interface, PHOC_LAYER_SHELL_EFFECTS_PROTOCOL_NAME) == 0) {
+    self->phoc_layer_shell_effects_version = version;
+  } else if (strcmp (interface, PHOSH_PRIVATE_PROTOCOL_NAME) == 0) {
+    self->phosh_private_version = version;
   }
 
   if (self->foreign_toplevel_manager && self->output_manager &&
@@ -199,6 +210,7 @@ mobile_settings_application_show_about (GSimpleAction *action,
                          "copyright", "Copyright (C) 2022 Guido Günther",
                          "website", "https://gitlab.gnome.org/guidog/phosh-mobile-settings",
                          "issue-url", "https://gitlab.gnome.org/guidog/phosh-mobile-settings/-/issues/new",
+                         "debug-info", mobile_settings_generate_debug_info (),
                          "license-type", GTK_LICENSE_GPL_3_0,
                          "developers", developers,
                          "artists", artists,
@@ -259,4 +271,19 @@ mobile_settings_application_get_head_tracker (MobileSettingsApplication *self)
   g_assert (MOBILE_SETTINGS_APPLICATION (self));
 
   return self->head_tracker;
+}
+
+uint32_t
+mobile_settings_application_get_phoc_layer_shell_effects_version (MobileSettingsApplication *self)
+{
+  g_assert (MOBILE_SETTINGS_APPLICATION (self));
+
+  return self->phoc_layer_shell_effects_version;
+}
+
+uint32_t
+mobile_settings_application_get_phosh_private_version (MobileSettingsApplication *self){
+  g_assert (MOBILE_SETTINGS_APPLICATION (self));
+
+  return self->phosh_private_version;
 }

src/mobile-settings-debug-info.c
@@ -1,0 +1,195 @@
+/*
+ * Copyright 2023 Gotam Gorabh <gautamy672@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#include "mobile-settings-config.h"
+
+#include "mobile-settings-application.h"
+#include "mobile-settings-debug-info.h"
+
+/* Copied and adapted from gtk/inspector/general.c */
+static void
+get_gtk_info (const char **backend,
+              const char **renderer)
+{
+  GdkDisplay *display = gdk_display_get_default ();
+  GdkSurface *surface = NULL;
+  GskRenderer *gsk_renderer = NULL;
+
+  if (!g_strcmp0 (G_OBJECT_TYPE_NAME (display), "GdkX11Display"))
+    *backend = "X11";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (display), "GdkWaylandDisplay"))
+    *backend = "Wayland";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (display), "GdkBroadwayDisplay"))
+    *backend = "Broadway";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (display), "GdkWin32Display"))
+    *backend = "Windows";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (display), "GdkMacosDisplay"))
+    *backend = "macOS";
+  else
+    *backend = G_OBJECT_TYPE_NAME (display);
+
+  surface = gdk_surface_new_toplevel (display);
+  gsk_renderer = gsk_renderer_new_for_surface (surface);
+  if (!g_strcmp0 (G_OBJECT_TYPE_NAME (gsk_renderer), "GskVulkanRenderer"))
+    *renderer = "Vulkan";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (gsk_renderer), "GskGLRenderer"))
+    *renderer = "GL";
+  else if (!g_strcmp0 (G_OBJECT_TYPE_NAME (gsk_renderer), "GskCairoRenderer"))
+    *renderer = "Cairo";
+  else
+    *renderer = G_OBJECT_TYPE_NAME (gsk_renderer);
+
+  gsk_renderer_unrealize (gsk_renderer);
+  g_object_unref (gsk_renderer);
+  gdk_surface_destroy (surface);
+}
+
+#ifndef G_OS_WIN32
+static char *
+get_flatpak_info (const char *group,
+                  const char *key)
+{
+  GKeyFile *keyfile = g_key_file_new ();
+  g_autofree char *ret = NULL;
+
+  if (g_key_file_load_from_file (keyfile, "/.flatpak-info", 0, NULL))
+    ret = g_key_file_get_string (keyfile, group, key, NULL);
+
+  g_key_file_unref (keyfile);
+
+  return ret;
+}
+#endif
+
+char *
+mobile_settings_generate_debug_info (void)
+{
+  GString *string = g_string_new (NULL);
+#ifndef G_OS_WIN32
+  gboolean flatpak = g_file_test ("/.flatpak-info", G_FILE_TEST_EXISTS);
+#endif
+
+  g_string_append_printf (string, "Mobile Settings: %s\n", MOBILE_SETTINGS_VERSION);
+  /*Adding Wayland Protocol*/
+  {
+    MobileSettingsApplication *app = MOBILE_SETTINGS_APPLICATION (g_application_get_default ());
+
+    g_string_append (string, "\n");
+    g_string_append (string, "Wayland Protocols:\n");
+    g_string_append_printf (string, "- phoc-layer-shell-effects: %d\n",
+                            mobile_settings_application_get_phoc_layer_shell_effects_version (app));
+    g_string_append_printf (string, "- phosh-private: %d\n",
+                            mobile_settings_application_get_phosh_private_version (app));
+  }
+
+  g_string_append (string, "\n");
+
+  g_string_append (string, "Compiled against:\n");
+  g_string_append_printf (string, "- GLib: %d.%d.%d\n", GLIB_MAJOR_VERSION,
+                                                        GLIB_MINOR_VERSION,
+                                                        GLIB_MICRO_VERSION);
+  g_string_append_printf (string, "- GTK: %d.%d.%d\n", GTK_MAJOR_VERSION,
+                                                       GTK_MINOR_VERSION,
+                                                       GTK_MICRO_VERSION);
+
+  g_string_append_printf (string, "- Libadwaita: %d.%d.%d\n", ADW_MAJOR_VERSION,
+                                                              ADW_MINOR_VERSION,
+                                                              ADW_MICRO_VERSION);
+  g_string_append (string, "\n");
+
+  g_string_append (string, "Running against:\n");
+  g_string_append_printf (string, "- GLib: %d.%d.%d\n", glib_major_version,
+                                                        glib_minor_version,
+                                                        glib_micro_version);
+  g_string_append_printf (string, "- GTK: %d.%d.%d\n", gtk_get_major_version (),
+                                                       gtk_get_minor_version (),
+                                                       gtk_get_micro_version ());
+
+  g_string_append_printf (string, "- Libadwaita: %d.%d.%d\n", adw_get_major_version (),
+                                                              adw_get_minor_version (),
+                                                              adw_get_micro_version ());
+  g_string_append (string, "\n");
+
+  {
+    g_autofree char *os_name = g_get_os_info (G_OS_INFO_KEY_NAME);
+    g_autofree char *os_version = g_get_os_info (G_OS_INFO_KEY_VERSION);
+
+    g_string_append (string, "System:\n");
+    g_string_append_printf (string, "- Name: %s\n", os_name);
+    g_string_append_printf (string, "- Version: %s\n", os_version);
+    g_string_append (string, "\n");
+  }
+
+  {
+    const char *backend, *renderer;
+
+    get_gtk_info (&backend, &renderer);
+
+
+    g_string_append (string, "GTK:\n");
+    g_string_append_printf (string, "- GDK backend: %s\n", backend);
+    g_string_append_printf (string, "- GSK renderer: %s\n", renderer);
+    g_string_append (string, "\n");
+  }
+
+#ifndef G_OS_WIN32
+  if (flatpak) {
+    g_autofree char *runtime = get_flatpak_info ("Application", "runtime");
+    g_autofree char *runtime_commit = get_flatpak_info ("Instance", "runtime-commit");
+    g_autofree char *arch = get_flatpak_info ("Instance", "arch");
+    g_autofree char *flatpak_version = get_flatpak_info ("Instance", "flatpak-version");
+    g_autofree char *devel = get_flatpak_info ("Instance", "devel");
+
+    g_string_append (string, "Flatpak:\n");
+    g_string_append_printf (string, "- Runtime: %s\n", runtime);
+    g_string_append_printf (string, "- Runtime commit: %s\n", runtime_commit);
+    g_string_append_printf (string, "- Arch: %s\n", arch);
+    g_string_append_printf (string, "- Flatpak version: %s\n", flatpak_version);
+    g_string_append_printf (string, "- Devel: %s\n", devel ? "yes" : "no");
+    g_string_append (string, "\n");
+  }
+#endif
+
+  {
+    const char *phoc_debug = g_getenv("PHOC_DEBUG");
+    const char *phosh_debug = g_getenv("PHOSH_DEBUG");
+    const char *desktop = g_getenv ("XDG_CURRENT_DESKTOP");
+    const char *session_desktop = g_getenv ("XDG_SESSION_DESKTOP");
+    const char *session_type = g_getenv ("XDG_SESSION_TYPE");
+    const char *lang = g_getenv ("LANG");
+    const char *builder = g_getenv ("INSIDE_GNOME_BUILDER");
+    const char *gtk_debug = g_getenv ("GTK_DEBUG");
+    const char *gtk_theme = g_getenv ("GTK_THEME");
+    const char *adw_debug_color_scheme = g_getenv ("ADW_DEBUG_COLOR_SCHEME");
+    const char *adw_debug_high_contrast = g_getenv ("ADW_DEBUG_HIGH_CONTRAST");
+    const char *adw_disable_portal = g_getenv ("ADW_DISABLE_PORTAL");
+
+    g_string_append (string, "Environment:\n");
+    g_string_append_printf (string, "- Desktop: %s\n", desktop);
+    g_string_append_printf (string, "- Session: %s (%s)\n", session_desktop,
+                                                            session_type);
+    g_string_append_printf (string, "- Language: %s\n", lang);
+    g_string_append_printf (string, "- Running inside Builder: %s\n", builder ? "yes" : "no");
+
+    if (phoc_debug)
+      g_string_append_printf (string, "- PHOC_DEBUG: %s\n", phoc_debug);
+    if (phosh_debug)
+      g_string_append_printf (string, "- PHOSH_DEBUG: %s\n", phosh_debug);
+    if (gtk_debug)
+      g_string_append_printf (string, "- GTK_DEBUG: %s\n", gtk_debug);
+    if (gtk_theme)
+      g_string_append_printf (string, "- GTK_THEME: %s\n", gtk_theme);
+    if (adw_debug_color_scheme)
+      g_string_append_printf (string, "- ADW_DEBUG_COLOR_SCHEME: %s\n", adw_debug_color_scheme);
+    if (adw_debug_high_contrast)
+      g_string_append_printf (string, "- ADW_DEBUG_HIGH_CONTRAST: %s\n", adw_debug_high_contrast);
+    if (adw_disable_portal)
+      g_string_append_printf (string, "- ADW_DISABLE_PORTAL: %s\n", adw_disable_portal);
+  }
+
+  return g_string_free_and_steal (string);
+}
+

src/mobile-settings-debug-info.h
@@ -1,0 +1,15 @@
+/*
+ * Copyright 2023 Gotam Gorabh <gautamy672@gmail.com>
+ *
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+char *mobile_settings_generate_debug_info (void);
+
+G_END_DECLS

src/meson.build
@@ -16,6 +16,8 @@ mobile_settings_sources = [
   'mobile-settings-window.c',
   'mobile-settings-application.h',
   'mobile-settings-application.c',
+  'mobile-settings-debug-info.h',
+  'mobile-settings-debug-info.c',
   'ms-compositor-panel.c',
   'ms-compositor-panel.h',
   'ms-convergence-panel.c',

src/mobile-settings-application.h
@@ -24,4 +24,6 @@ MobileSettingsApplication *mobile_settings_application_new (gchar *application_i
 GtkWidget *mobile_settings_application_get_device_panel  (MobileSettingsApplication *self);
 MsToplevelTracker *mobile_settings_application_get_toplevel_tracker (MobileSettingsApplication *self);
 MsHeadTracker     *mobile_settings_application_get_head_tracker (MobileSettingsApplication *self);
+uint32_t mobile_settings_application_get_phoc_layer_shell_effects_version (MobileSettingsApplication *self);
+uint32_t mobile_settings_application_get_phosh_private_version (MobileSettingsApplication *self);
 G_END_DECLS

commit 37d10a1ffdceb7a3054f62831608d9bed7ad0b49

ms-settings-debug-info: Use correct namespace
Fixes: 8e9df9d ("Add Troubleshooting Tab")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/289>

src/mobile-settings-application.c
@@ -210,7 +210,7 @@ print_system_information (GApplication *app)
   wl_display_roundtrip (self->wl_display);
   wl_display_dispatch (self->wl_display);
 
-  debug_info = mobile_settings_generate_debug_info ();
+  debug_info = ms_generate_debug_info ();
   g_print ("Debugging information:\n%s", debug_info);
 }
 
@@ -445,7 +445,7 @@ ms_application_show_about (GSimpleAction *action,
   adw_about_dialog_set_translator_credits (about_dialog,
                /* Translators: Replace "translator-credits" with your names, one name per line */
                                            _("translator-credits"));
-  adw_about_dialog_set_debug_info (about_dialog, mobile_settings_generate_debug_info ());
+  adw_about_dialog_set_debug_info (about_dialog, ms_generate_debug_info ());
 
   window = gtk_application_get_active_window (GTK_APPLICATION (self));
   adw_dialog_present (ADW_DIALOG (about_dialog), GTK_WIDGET (window));

src/mobile-settings-debug-info.c
@@ -195,7 +195,7 @@ append_display_configuration (GString *string)
 
 
 char *
-mobile_settings_generate_debug_info (void)
+ms_generate_debug_info (void)
 {
   GString *string = g_string_new (NULL);
   gboolean flatpak = g_file_test ("/.flatpak-info", G_FILE_TEST_EXISTS);

src/mobile-settings-debug-info.h
@@ -10,6 +10,6 @@
 
 G_BEGIN_DECLS
 
-char *mobile_settings_generate_debug_info (void);
+char *ms_generate_debug_info (void);
 
 G_END_DECLS