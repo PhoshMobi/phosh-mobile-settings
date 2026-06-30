PhoshMobi/phosh-mobile-settings

commit 3c31bddca874b370e3eba910f0ff88e7a441be3c

ms-panel: Create base class for standard panels
A new base class will make it easier for example
to set common properties/signals.

Signed-off-by: Rudra Pratap Singh <rudrasingh900@gmail.com>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/216>

src/meson.build
@@ -60,6 +60,8 @@ mobile_settings_sources = [
   'ms-osk-panel.h',
   'ms-overview-panel.c',
   'ms-overview-panel.h',
+  'ms-panel.c',
+  'ms-panel.h',
   'ms-panel-switcher.c',
   'ms-panel-switcher.h',
   'ms-plugin-loader.h',

src/ms-panel.c
@@ -1,0 +1,142 @@
+/*
+ * Copyright (C) 2025 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * Author: Rudra Pratap Singh <rudrasingh900@gmail.com>
+ */
+
+#include "ms-panel.h"
+#include "ms-util.h"
+
+enum {
+  PROP_0,
+  PROP_KEYWORDS,
+  PROP_LAST_PROP
+};
+static GParamSpec *props[PROP_LAST_PROP];
+
+/**
+ * MsPanel:
+ *
+ * Base class for standard panels. Panel implementations need to derive
+ * from this class.
+ */
+typedef struct {
+  GtkStringList *keywords;
+} MsPanelPrivate;
+
+G_DEFINE_ABSTRACT_TYPE_WITH_PRIVATE (MsPanel, ms_panel, ADW_TYPE_BIN)
+
+
+static void
+ms_panel_set_property (GObject      *object,
+                       guint         property_id,
+                       const GValue *value,
+                       GParamSpec   *pspec)
+{
+  MsPanel *self = MS_PANEL (object);
+
+  switch (property_id) {
+  case PROP_KEYWORDS:
+    ms_panel_set_keywords (self, g_value_get_object (value));
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+ms_panel_get_property (GObject    *object,
+                       guint       property_id,
+                       GValue     *value,
+                       GParamSpec *pspec)
+{
+  MsPanel *self = MS_PANEL (object);
+  MsPanelPrivate *priv = ms_panel_get_instance_private (self);
+
+  switch (property_id) {
+  case PROP_KEYWORDS:
+    g_value_set_object (value, priv->keywords);
+    break;
+  default:
+    G_OBJECT_WARN_INVALID_PROPERTY_ID (object, property_id, pspec);
+    break;
+  }
+}
+
+
+static void
+ms_panel_finalize (GObject *object)
+{
+  MsPanel *self = MS_PANEL (object);
+  MsPanelPrivate *priv = ms_panel_get_instance_private (self);
+
+  g_clear_object (&priv->keywords);
+
+  G_OBJECT_CLASS (ms_panel_parent_class)->finalize (object);
+}
+
+
+static void
+ms_panel_class_init (MsPanelClass *klass)
+{
+  GObjectClass *object_class = G_OBJECT_CLASS (klass);
+
+  object_class->set_property = ms_panel_set_property;
+  object_class->get_property = ms_panel_get_property;
+  object_class->finalize = ms_panel_finalize;
+
+  /**
+   * MsPanel:keywords:
+   *
+   * The string keyword(s) associated with the panel.
+   */
+  props[PROP_KEYWORDS] =
+    g_param_spec_object ("keywords", "", "",
+                         GTK_TYPE_STRING_LIST,
+                         G_PARAM_READWRITE | G_PARAM_EXPLICIT_NOTIFY | G_PARAM_STATIC_STRINGS);
+
+  g_object_class_install_properties (object_class, PROP_LAST_PROP, props);
+}
+
+
+static void
+ms_panel_init (MsPanel *self)
+{
+}
+
+
+GtkStringList *
+ms_panel_get_keywords (MsPanel *self)
+{
+  MsPanelPrivate *priv;
+
+  g_return_val_if_fail (MS_IS_PANEL (self), NULL);
+
+  priv = ms_panel_get_instance_private (self);
+
+  return priv->keywords;
+}
+
+
+void
+ms_panel_set_keywords (MsPanel *self, GtkStringList *keywords)
+{
+  MsPanelPrivate *priv;
+
+  g_return_if_fail (MS_IS_PANEL (self));
+  g_return_if_fail (GTK_IS_STRING_LIST (keywords));
+
+  priv = ms_panel_get_instance_private (self);
+
+  if (priv->keywords == keywords)
+    return;
+
+  g_clear_object (&priv->keywords);
+  priv->keywords = ms_get_casefolded_string_list (keywords);
+
+  g_object_notify_by_pspec (G_OBJECT (self), props[PROP_KEYWORDS]);
+}

src/ms-panel.h
@@ -1,0 +1,24 @@
+/*
+ * Copyright (C) 2025 The Phosh Developers
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <adwaita.h>
+
+G_BEGIN_DECLS
+
+#define MS_TYPE_PANEL (ms_panel_get_type ())
+
+G_DECLARE_DERIVABLE_TYPE (MsPanel, ms_panel, MS, PANEL, AdwBin)
+
+struct _MsPanelClass {
+  AdwBinClass parent_class;
+};
+
+GtkStringList *ms_panel_get_keywords (MsPanel *self);
+void           ms_panel_set_keywords (MsPanel *self, GtkStringList *keywords);
+
+G_END_DECLS

commit 7bb554ad93bb4865a5d605c49650a818dffddf9f

plugin-panel: Base on MsPanel
All panels must use `MsPanel` as base class

Fixes: 3c31bddc ("ms-panel: Create base class for standard panels")
Signed-off-by: Guido Günther <agx@sigxcpu.org>
Part-of: <https://gitlab.gnome.org/World/Phosh/phosh-mobile-settings/-/merge_requests/374>

plugins/librem5/ms-plugin-librem5-panel.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
+ *               2026 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *

src/meson.build
@@ -65,7 +65,6 @@ mobile_settings_sources = [
   'ms-plugin-list-box.h',
   'ms-plugin-loader.c',
   'ms-plugin-loader.h',
-  'ms-plugin-loader.h',
   'ms-plugin-row.c',
   'ms-plugin-row.h',
   'ms-scale-to-fit-row.c',

src/ms-plugin-panel.c
@@ -1,11 +1,13 @@
 /*
  * Copyright (C) 2022 Purism SPC
+ *               2023-2026 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
  * Author: Guido Günther <agx@sigxcpu.org>
  */
 
+#include "ms-panel.h"
 #include "ms-plugin-panel.h"
 
 enum {
@@ -25,7 +27,7 @@ typedef struct _MsPluginPanelPrivate {
   char *title;
 } MsPluginPanelPrivate;
 
-G_DEFINE_TYPE_WITH_PRIVATE (MsPluginPanel, ms_plugin_panel, ADW_TYPE_BIN)
+G_DEFINE_TYPE_WITH_PRIVATE (MsPluginPanel, ms_plugin_panel, MS_TYPE_PANEL)
 
 
 static void

src/ms-plugin-panel.h
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2022 Purism SPC
+ *               2023-2026 Phosh.mobi e.V.
  *
  * SPDX-License-Identifier: GPL-3.0-or-later
  *
@@ -8,7 +9,7 @@
 
 #pragma once
 
-#include "ms-plugin-panel.h"
+#include "ms-panel.h"
 
 #include <adwaita.h>
 
@@ -16,11 +17,11 @@ G_BEGIN_DECLS
 
 #define MS_TYPE_PLUGIN_PANEL (ms_plugin_panel_get_type ())
 
-G_DECLARE_DERIVABLE_TYPE (MsPluginPanel, ms_plugin_panel, MS, PLUGIN_PANEL, AdwBin)
+G_DECLARE_DERIVABLE_TYPE (MsPluginPanel, ms_plugin_panel, MS, PLUGIN_PANEL, MsPanel)
 
 
 struct _MsPluginPanelClass {
-  AdwBinClass parent_class;
+  MsPanelClass parent_class;
 };
 
 